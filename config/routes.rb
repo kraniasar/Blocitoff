9e87532c7e70c58efa75f9183e50e20af5c9b94e
Amanda <kraniasar@gmail.com>
2016-03-04 18:07:55 -0500

    Added ability to create items

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  root :to => 'welcome#index'
 
   get 'welcome/index'
 
   get 'welcome/about'
 
-  get 'users/show'
-
   devise_for :users
 
-  root 'welcome#index'
+  resources :users, only: [:show] do
+    resources :items, only: [:create, :destroy]
+  end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224040143) do
+ActiveRecord::Schema.define(version: 20160226154754) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
